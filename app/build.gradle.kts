8d98c85b80f1234f5a39338602794f9806ab13fe
Ricardo Perello <ricardo.perellomas@epfl.ch>
2024-10-04 17:17:27 +0200

    git commit -m "feat(barcode): integrate Google ML Kit for barcode scanning"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,7 +122,11 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
 
 
-
+    //Barcode Scanner
+    implementation(libs.mlkit.barcode.scanning)
+    implementation(libs.camerax.camera2)
+    implementation(libs.camerax.lifecycle)
+    implementation(libs.camerax.view)
 
 
     // Navigation
