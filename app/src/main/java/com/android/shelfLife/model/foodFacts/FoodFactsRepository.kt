524ecf083c6df37449711196067da70d1e2a407d
alextheprophecy <alexandrebourgoin23@gmail.com>
2024-10-07 19:26:27 +0200

    feat: create foodFacts repository and OpenFoodFacts repository implementation
    Using OkHttp to query openfoodfacts web api and extract the foodfacts from the json result

---
+++ app/src/main/java/com/android/shelfLife/model/foodFacts/FoodFactsRepository.kt
@@ -0,0 +1,18 @@
+package com.android.shelfLife.model.foodFacts
+
+// Sealed class to represent different types of search inputs
+sealed class FoodSearchInput {
+    data class Barcode(val barcode: Int) : FoodSearchInput()
+    data class Query(val searchQuery: String) : FoodSearchInput()
+}
+
+interface FoodFactsRepository {
+    /**
+     * Searches for food facts based on the provided search input.
+     *
+     * @param searchInput The search input to use for querying food facts.
+     * @param onSuccess Callback function to handle successful search results.
+     * @param onFailure Callback function to handle search failures.
+     */
+    fun searchFoodFacts(searchInput: FoodSearchInput, onSuccess: (List<FoodFacts>) -> Unit, onFailure: (Exception) -> Unit)
+}
