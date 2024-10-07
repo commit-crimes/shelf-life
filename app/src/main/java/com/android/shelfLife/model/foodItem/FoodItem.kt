8bd4add7cf1c85c5db6b347676a5d14cfa6d0c76
alextheprophecy <alextheprophecy@gmail.com>
2024-10-07 13:14:35 +0200

    feat: update FoodItem data model to cover more parameters
    Updated the fooditem model to contain parameters discussed during the scrum meeting on the 07.10.

--- app/src/main/java/com/android/shelfLife/model/foodItem/FoodItem.kt
+++ app/src/main/java/com/android/shelfLife/model/foodItem/FoodItem.kt
@@ -5,30 +5,42 @@ import com.google.firebase.Timestamp
 data class FoodItem(
     val uid: String, // Unique ID of the food item
     val name: String, // Name of the food item
-    val barcode: String, // Barcode number, default to an empty string if not provided
+    val barcode: String = "", // Barcode number, default to an empty string if not provided
     val quantity: Quantity, // Quantity of the food item
+    val status: FoodStatus = FoodStatus.CLOSED, // Default status is CLOSED
+    val nutritionFacts: NutritionFacts = NutritionFacts() // Default empty NutritionFacts object,
+    val foodCategory: FoodCategory = FoodCategory.OTHER, // Default category is OTHER
+    val location: FoodLocation = FoodLocation(0, _Location.PANTRY), // Default location is PANTRY
     val expiryDate: Timestamp? = null, // Expiry date can be null if not provided
+    val openDate: Timestamp? = null, // Expiry date can be null if not provided
     val buyDate: Timestamp = Timestamp.now(), // Default buy date is the current time
-    val status: FoodItemStatus = FoodItemStatus.CLOSED, // Default status is CLOSED
-    val nutritionFacts: NutritionFacts = NutritionFacts() // Default empty NutritionFacts object
 )
 
 data class Quantity(
     val amount: Double, // Amount of the quantity
-    val unit: FoodUnit = FoodUnit.GRAM // Unit of the quantity
+    val unit: _Unit = _Unit.GRAM // Unit of the quantity
 )
 
-enum class FoodUnit {
-    GRAM, // Gram unit
-    ML, // Milliliter unit
-    COUNT // Item count (2 pineapples, 3 apples, etc.)
+data class FoodLocation(
+    val householdNumber: Int, // Household number
+    val location: _Location // Location can be PANTRY, FRIDGE, or FREEZER
+)
+
+enum class FoodCategory {
+    FRUIT, // Fruit category
+    VEGETABLE, // Vegetable category
+    MEAT, // Meat category
+    DAIRY, // Dairy category
+    GRAIN, // Grain category
+    BEVERAGE, // Beverage category
+    SNACK, // Snack category
+    OTHER // Other category
 }
 
 /** This enum class represents the status of a food item. */
-enum class FoodItemStatus {
+enum class FoodStatus {
     OPEN, // Food item is opened
     CLOSED, // Food item is closed or sealed
-    FROZEN, // Food item is frozen
     EXPIRED // Food item has expired
 }
 
@@ -45,3 +57,16 @@ data class NutritionFacts(
     val proteins: Double = 0.0, // Default proteins value is 0.0
     val salt: Double = 0.0, // Default salt value is 0.0
 )
+
+
+enum class _Location {
+    PANTRY, // Pantry location
+    FRIDGE, // Fridge location
+    FREEZER, // Freezer location
+}
+
+enum class _Unit {
+    GRAM, // Gram unit
+    ML, // Milliliter unit
+    COUNT // Item count (2 pineapples, 3 apples, etc.)
+}
