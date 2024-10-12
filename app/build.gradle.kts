487efb6f4e2d90b0a38af1a76758c3cd3d20e6a2
alextheprophecy <alexandrebourgoin23@gmail.com>
2024-10-12 14:39:01 +0200

    feat(openai API KEY): setup Open AI client and API key in build config
    setup the dependencies for the OpenaiAi API client and the usage of our private API key.
    
    WARNING! remember to add OPENAI_API_KEY=<key> in local.properties for the openai api to work!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,8 @@ android {
         localProperties.load(FileInputStream(localPropertiesFile))
     }
 
+    val openAIApiKey: String = localProperties.getProperty("OPENAI_API_KEY") ?: ""
+
     defaultConfig {
         applicationId = "com.android.shelfLife"
         minSdk = 24
@@ -32,6 +34,13 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+
+        // Add the API key to the BuildConfig class
+        buildConfigField(
+            type = "String",
+            name = "OPENAI_API_KEY",
+            value = "\"${openAIApiKey}\""
+        )
     }
     testOptions {
         unitTests {
@@ -61,6 +70,7 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
     composeOptions {
@@ -152,6 +162,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.material.icons.extended)
 
+    //Openai
+    implementation(libs.aallam.openai.client)
+
 
     //Barcode Scanner
     implementation(libs.mlkit.barcode.scanning)
@@ -176,7 +189,6 @@ dependencies {
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.auth)
     implementation(platform(libs.firebase.bom))
-
 
     // Networking with OkHttp
     implementation(libs.okhttp)
