c382de7109cfac801be01bf26b8329d0b9e01773
alextheprophecy <alexandrebourgoin23@gmail.com>
2024-10-15 14:51:16 +0200

    refactor(recipe): change instructions from a single string to a list of FoodItems
    -reason: by saving this as a list of steps instead, we allow for more flexibility and features (streaming recipe instruction by instruction from api, fancy instruction animations on UI etc.)

--- app/src/main/java/com/android/shelfLife/model/recipe/Recipe.kt
+++ app/src/main/java/com/android/shelfLife/model/recipe/Recipe.kt
@@ -6,7 +6,7 @@ import kotlin.time.Duration
 
 data class Recipe(
     val name: String, // name of recipe
-    val instructions: String, // instructions of recipes
+    val instructions: List<String>, // instructions of recipes step by step (hence the list)
     val servings: Int, // total number of servings
     val time: Duration, // time it take to cook
     val ingredients : List<FoodItem> = listOf(), // ingredients in recipe todo check with Alex about this
