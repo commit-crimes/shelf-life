1bf630badf9b8f6a9f539adb7718aa77132d33cc
applecognac <kevinyangzhang2934@gmail.com>
2024-10-15 21:40:11 +0200

    feat: Integrate the AddFoodItem to display error messages when the form is not filled out properly

--- app/src/main/java/com/android/shelfLife/ui/overview/AddFoodItem.kt
+++ app/src/main/java/com/android/shelfLife/ui/overview/AddFoodItem.kt
@@ -2,11 +2,13 @@ package com.android.shelfLife.ui.overview
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.android.shelfLife.model.foodFacts.FoodCategory
@@ -19,9 +21,22 @@ import com.android.shelfLife.model.foodItem.FoodStorageLocation
 import com.android.shelfLife.model.foodItem.ListFoodItemsViewModel
 import com.android.shelfLife.model.household.HouseholdViewModel
 import com.android.shelfLife.ui.navigation.NavigationActions
+import com.android.shelfLife.ui.utils.ErrorPopUp
+import com.example.compose.primaryContainerLight
 import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
 import java.util.*
+
+/**
+ * A composable function that displays the screen for adding a new food item to the user's
+ * household inventory.
+ *
+ * @param navigationActions An instance of [NavigationActions] that handles navigation actions.
+ * @param houseHoldViewModel An instance of [HouseholdViewModel] that provides access to the user's
+ * household data.
+ * @param foodItemViewModel An instance of [ListFoodItemsViewModel] that provides access to the user's
+ * food item data.
+ */
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,188 +45,248 @@ fun AddFoodItemScreen(
     houseHoldViewModel: HouseholdViewModel,
     foodItemViewModel: ListFoodItemsViewModel
 ) {
+    var foodName by remember { mutableStateOf("") }
+    var amount by remember { mutableStateOf("") }
+    var unit by remember { mutableStateOf(FoodUnit.GRAM) }
+    var category by remember { mutableStateOf(FoodCategory.OTHER) }
+    var location by remember { mutableStateOf(FoodStorageLocation.PANTRY) }
+    var expireDate by remember { mutableStateOf("") }
+    var openDate by remember { mutableStateOf("") }
+    var buyDate by remember { mutableStateOf(formatTimestampToDate(Timestamp.now())) }
 
-  var foodName by remember { mutableStateOf("") }
-  var amount by remember { mutableStateOf("") }
-  var unit by remember { mutableStateOf(FoodUnit.GRAM) }
-  var category by remember { mutableStateOf(FoodCategory.OTHER) }
-  var location by remember { mutableStateOf(FoodStorageLocation.PANTRY) }
-  var expireDate by remember { mutableStateOf("") }
-  var openDate by remember { mutableStateOf("") }
-  var buyDate by remember { mutableStateOf(formatTimestampToDate(Timestamp.now())) }
+    val errorMessages by remember { mutableStateOf(mutableMapOf<String, String>()) }
+    var showDialog by remember { mutableStateOf(false) }
+    var unitExpanded by remember { mutableStateOf(false) }
+    var categoryExpanded by remember { mutableStateOf(false) }
+    var locationExpanded by remember { mutableStateOf(false) }
 
-  var unitExpanded by remember { mutableStateOf(false) }
-  var categoryExpanded by remember { mutableStateOf(false) }
-  var locationExpanded by remember { mutableStateOf(false) }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopAppBar(title = { Text("Add Food Item") }) },
+    ) { padding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(padding).padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            OutlinedTextField(
+                value = foodName,
+                onValueChange = { foodName = it },
+                label = { Text("Name of food") },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+            )
 
-  Scaffold(
-      modifier = Modifier.fillMaxSize(),
-      topBar = { TopAppBar(title = { Text("Add Food Item") }) },
-  ) { padding ->
-    Column(
-        modifier = Modifier.fillMaxSize().padding(padding).padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top) {
-          OutlinedTextField(
-              value = foodName,
-              onValueChange = { foodName = it },
-              label = { Text("Name of food") },
-              modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp))
-
-          Row(
-              modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
-              horizontalArrangement = Arrangement.SpaceBetween) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 OutlinedTextField(
                     value = amount,
                     onValueChange = { amount = it },
                     label = { Text("Amount") },
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    modifier = Modifier.weight(1f).padding(end = 8.dp))
+                    modifier = Modifier.weight(1f).padding(end = 8.dp)
+                )
 
                 ExposedDropdownMenuBox(
                     expanded = unitExpanded,
                     onExpandedChange = { unitExpanded = !unitExpanded },
-                    modifier = Modifier.weight(1f)) {
-                      OutlinedTextField(
-                          value = unit.name.lowercase(),
-                          onValueChange = {},
-                          label = { Text("Unit") },
-                          readOnly = true,
-                          trailingIcon = {
+                    modifier = Modifier.weight(1f)
+                ) {
+                    OutlinedTextField(
+                        value = fromCapitalStringtoLowercaseString(unit.name),
+                        onValueChange = {},
+                        label = { Text("Unit") },
+                        readOnly = true,
+                        trailingIcon = {
                             ExposedDropdownMenuDefaults.TrailingIcon(expanded = unitExpanded)
-                          },
-                          modifier = Modifier.menuAnchor())
-                      ExposedDropdownMenu(
-                          expanded = unitExpanded, onDismissRequest = { unitExpanded = false }) {
-                            FoodUnit.values().forEach { selectionOption ->
-                              DropdownMenuItem(
-                                  text = { Text(selectionOption.name.lowercase()) },
-                                  onClick = {
+                        },
+                        modifier = Modifier.menuAnchor()
+                    )
+                    ExposedDropdownMenu(
+                        expanded = unitExpanded, onDismissRequest = { unitExpanded = false }
+                    ) {
+                        FoodUnit.values().forEach { selectionOption ->
+                            DropdownMenuItem(
+                                text = { Text(fromCapitalStringtoLowercaseString(selectionOption.name)) },
+                                onClick = {
                                     unit = selectionOption
                                     unitExpanded = false // Close dropdown
-                                  })
-                            }
-                          }
+                                }
+                            )
+                        }
                     }
-              }
+                }
+            }
 
-          // Category dropdown
-          ExposedDropdownMenuBox(
-              expanded = categoryExpanded,
-              onExpandedChange = { categoryExpanded = !categoryExpanded }) {
+            // Category dropdown
+            ExposedDropdownMenuBox(
+                expanded = categoryExpanded,
+                onExpandedChange = { categoryExpanded = !categoryExpanded }
+            ) {
                 OutlinedTextField(
-                    value = category.name.lowercase(),
+                    value = fromCapitalStringtoLowercaseString(category.name),
                     onValueChange = {},
                     label = { Text("Category") },
                     readOnly = true,
                     trailingIcon = {
-                      ExposedDropdownMenuDefaults.TrailingIcon(expanded = categoryExpanded)
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = categoryExpanded)
                     },
-                    modifier = Modifier.fillMaxWidth().menuAnchor())
+                    modifier = Modifier.fillMaxWidth().menuAnchor()
+                )
                 ExposedDropdownMenu(
-                    expanded = categoryExpanded, onDismissRequest = { categoryExpanded = false }) {
-                      FoodCategory.values().forEach { selectionOption ->
+                    expanded = categoryExpanded, onDismissRequest = { categoryExpanded = false }
+                ) {
+                    FoodCategory.values().forEach { selectionOption ->
                         DropdownMenuItem(
-                            text = { Text(selectionOption.name.lowercase()) },
+                            text = { Text(fromCapitalStringtoLowercaseString(selectionOption.name)) },
                             onClick = {
-                              category = selectionOption
-                              categoryExpanded = false
-                            })
-                      }
+                                category = selectionOption
+                                categoryExpanded = false
+                            }
+                        )
                     }
-              }
+                }
+            }
 
-          Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-          ExposedDropdownMenuBox(
-              expanded = locationExpanded,
-              onExpandedChange = { locationExpanded = !locationExpanded }) {
+            ExposedDropdownMenuBox(
+                expanded = locationExpanded,
+                onExpandedChange = { locationExpanded = !locationExpanded }
+            ) {
                 OutlinedTextField(
-                    value = location.name.lowercase(),
+                    value = fromCapitalStringtoLowercaseString(location.name),
                     onValueChange = {},
                     label = { Text("Location") },
                     readOnly = true,
                     trailingIcon = {
-                      ExposedDropdownMenuDefaults.TrailingIcon(expanded = locationExpanded)
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = locationExpanded)
                     },
-                    modifier = Modifier.fillMaxWidth().menuAnchor())
+                    modifier = Modifier.fillMaxWidth().menuAnchor()
+                )
                 ExposedDropdownMenu(
-                    expanded = locationExpanded, onDismissRequest = { locationExpanded = false }) {
-                      FoodStorageLocation.values().forEach { selectionOption ->
+                    expanded = locationExpanded, onDismissRequest = { locationExpanded = false }
+                ) {
+                    FoodStorageLocation.values().forEach { selectionOption ->
                         DropdownMenuItem(
-                            text = { Text(selectionOption.name) },
+                            text = { Text(fromCapitalStringtoLowercaseString(selectionOption.name)) },
                             onClick = {
-                              location = selectionOption
-                              locationExpanded = false
-                            })
-                      }
+                                location = selectionOption
+                                locationExpanded = false
+                            }
+                        )
                     }
-              }
+                }
+            }
 
-          // For dates a future improvement could be having a calendar interfare rather than manual
-          // input
-          OutlinedTextField(
-              value = expireDate,
-              onValueChange = { expireDate = it },
-              label = { Text("Expire Date") },
-              placeholder = { Text("dd/mm/yyyy") },
-              modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
-          )
+            // For dates a future improvement could be having a calendar interfare rather than manual
+            // input
+            OutlinedTextField(
+                value = expireDate,
+                onValueChange = { expireDate = it },
+                label = { Text("Expire Date") },
+                placeholder = { Text("dd/mm/yyyy") },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
+            )
 
-          OutlinedTextField(
-              value = openDate,
-              onValueChange = { openDate = it },
-              label = { Text("Open Date") },
-              placeholder = { Text("dd/mm/yyyy") },
-              modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
-          )
+            OutlinedTextField(
+                value = openDate,
+                onValueChange = { openDate = it },
+                label = { Text("Open Date") },
+                placeholder = { Text("dd/mm/yyyy") },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
+            )
 
-          OutlinedTextField(
-              value = buyDate,
-              onValueChange = { buyDate = it },
-              label = { Text("Buy Date") },
-              placeholder = { Text("dd/mm/yyyy") },
-              modifier = Modifier.fillMaxWidth().padding(bottom = 32.dp),
-          )
+            OutlinedTextField(
+                value = buyDate,
+                onValueChange = { buyDate = it },
+                label = { Text("Buy Date") },
+                placeholder = { Text("dd/mm/yyyy") },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 32.dp),
+            )
 
-          Button(
-              onClick = {
-                val foodFacts =
-                    FoodFacts(
-                        name = foodName,
-                        barcode = "",
-                        quantity = Quantity(amount.toDouble(), unit),
-                        category = category,
-                        // nutritionFacts = NutritionFacts()
-                    )
-                val newFoodItem =
-                    FoodItem(
-                        uid = foodItemViewModel.getUID(),
-                        foodFacts = foodFacts,
-                        location = location,
-                        expiryDate = formatDateToTimestamp(expireDate),
-                        openDate = formatDateToTimestamp(openDate),
-                        buyDate = formatDateToTimestamp(buyDate),
-                        // Have to add logic to determine status depending on the dates given here/
-                        // should be calculated
-                        status = FoodStatus.CLOSED)
-                houseHoldViewModel.addFoodItem(newFoodItem)
-                navigationActions.goBack()
-              },
-              modifier = Modifier.fillMaxWidth().height(50.dp)) {
+            Button(
+                onClick = {
+                    errorMessages.clear()
+
+
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+                    try {
+                        val expireDateParsed = sdf.parse(expireDate)
+                        val openDateParsed = sdf.parse(openDate)
+
+                        //Error if the expiration date is before the open date
+                        if (expireDateParsed.before(openDateParsed)) {
+                            errorMessages["date"] = "Expiration date cannot be before the open date."
+                        }
+                    } catch (e: Exception) {
+                        errorMessages["dateFormat"] = "Invalid date format. Please use dd/mm/yyyy."
+                    }
+
+                    //Error is the food name field is empty
+                    if (foodName.isBlank()) {
+                        errorMessages["foodName"] = "Food name cannot be empty."
+                    }
+
+                    //Error is the food amount is blank or not a number
+                    if (amount.isBlank()) {
+                        errorMessages["amount"] = "Amount cannot be empty."
+                    } else if (amount.toDoubleOrNull() == null) {
+                        errorMessages["amountFormat"] = "Amount must be a number."
+                    }
+
+                    if (errorMessages.isNotEmpty()) {
+                        showDialog = true
+                    } else {
+                        val foodFacts =
+                            FoodFacts(
+                                name = foodName,
+                                barcode = "",
+                                quantity = Quantity(amount.toDouble(), unit),
+                                category = category
+                            )
+                        val newFoodItem =
+                            FoodItem(
+                                uid = foodItemViewModel.getUID(),
+                                foodFacts = foodFacts,
+                                location = location,
+                                expiryDate = formatDateToTimestamp(expireDate),
+                                openDate = formatDateToTimestamp(openDate),
+                                buyDate = formatDateToTimestamp(buyDate),
+                                status = FoodStatus.CLOSED
+                            )
+                        houseHoldViewModel.addFoodItem(newFoodItem)
+                        navigationActions.goBack()
+                    }
+                },
+                modifier = Modifier.fillMaxWidth().height(50.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = primaryContainerLight)
+            ) {
                 Text(text = "Submit", fontSize = 18.sp)
-              }
+            }
+
+            ErrorPopUp(
+                showDialog = showDialog,
+                onDismiss = { showDialog = false },
+                errorMessages = errorMessages.values.toList()
+            )
         }
-  }
+    }
 }
 
 fun formatTimestampToDate(timestamp: Timestamp): String {
-  val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-  return sdf.format(timestamp.toDate())
+    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    return sdf.format(timestamp.toDate())
 }
 
 fun formatDateToTimestamp(dateString: String): Timestamp {
-  val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-  val date = sdf.parse(dateString)
-  return Timestamp(date)
+    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    val date = sdf.parse(dateString)
+    return Timestamp(date)
+}
+
+fun fromCapitalStringtoLowercaseString(enum: String): String {
+    return enum.lowercase().replaceFirstChar { it.uppercase() }
 }
