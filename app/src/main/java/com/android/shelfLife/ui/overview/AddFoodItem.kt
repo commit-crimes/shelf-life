c5baeb5dc7e9c9f31a7ff521fe5da42c57971a62
kevin yang <62985083+frostymate@users.noreply.github.com>
2024-10-16 16:05:55 +0200

    Feat/18 add logic for error handling in add food item (#51)
    * feat: Add a ErrorPopUp composable to be able to display error messages
    
    * feat: Integrated the AddFoodItem to display error messages when the form is not filled out properly
    
    * fix: Added common used methods into the utils folder
    
    * fix: Add a Dropdown Field composable to make the AddFoodItem screen more modular.
    
    -Delete IntermediateFoodItem as it is not used
    
    * fix: Add to string.xml hard coded strings for Text fields
    
    * feat: Add Go Back Button to AddFoodItem Screen
    
    -Run code clean up
    
    * feat: Integrate the AddFoodItem to display error messages when the form is not filled out properly
    
    * fix: Add common used methods into the utils folder
    
    * fix: Add a Dropdown Field composable to make the AddFoodItem screen more modular.
    
    -Delete IntermediateFoodItem as it is not used
    
    * fix: Add to string.xml hard coded strings for Text fields
    
    * feat: Add Go Back Button to AddFoodItem Screen
    
    -Run code clean up
    
    * fix: Change parameter modifier_input into modifier in DropdownFields

--- app/src/main/java/com/android/shelfLife/ui/overview/AddFoodItem.kt
+++ app/src/main/java/com/android/shelfLife/ui/overview/AddFoodItem.kt
@@ -2,13 +2,18 @@ package com.android.shelfLife.ui.overview
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.android.shelfLife.R
 import com.android.shelfLife.model.foodFacts.FoodCategory
 import com.android.shelfLife.model.foodFacts.FoodFacts
 import com.android.shelfLife.model.foodFacts.FoodUnit
@@ -19,10 +24,26 @@ import com.android.shelfLife.model.foodItem.FoodStorageLocation
 import com.android.shelfLife.model.foodItem.ListFoodItemsViewModel
 import com.android.shelfLife.model.household.HouseholdViewModel
 import com.android.shelfLife.ui.navigation.NavigationActions
+import com.android.shelfLife.ui.utils.DropdownFields
+import com.android.shelfLife.ui.utils.ErrorPopUp
+import com.android.shelfLife.ui.utils.formatDateToTimestamp
+import com.android.shelfLife.ui.utils.formatTimestampToDate
+import com.android.shelfLife.ui.utils.fromCapitalStringtoLowercaseString
+import com.example.compose.primaryContainerLight
 import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
 import java.util.*
 
+/**
+ * A composable function that displays the screen for adding a new food item to the user's household
+ * inventory.
+ *
+ * @param navigationActions An instance of [NavigationActions] that handles navigation actions.
+ * @param houseHoldViewModel An instance of [HouseholdViewModel] that provides access to the user's
+ *   household data.
+ * @param foodItemViewModel An instance of [ListFoodItemsViewModel] that provides access to the
+ *   user's food item data.
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddFoodItemScreen(
@@ -30,7 +51,6 @@ fun AddFoodItemScreen(
     houseHoldViewModel: HouseholdViewModel,
     foodItemViewModel: ListFoodItemsViewModel
 ) {
-
   var foodName by remember { mutableStateOf("") }
   var amount by remember { mutableStateOf("") }
   var unit by remember { mutableStateOf(FoodUnit.GRAM) }
@@ -40,13 +60,23 @@ fun AddFoodItemScreen(
   var openDate by remember { mutableStateOf("") }
   var buyDate by remember { mutableStateOf(formatTimestampToDate(Timestamp.now())) }
 
+  val errorMessages by remember { mutableStateOf(mutableMapOf<String, String>()) }
+  var showDialog by remember { mutableStateOf(false) }
   var unitExpanded by remember { mutableStateOf(false) }
   var categoryExpanded by remember { mutableStateOf(false) }
   var locationExpanded by remember { mutableStateOf(false) }
 
   Scaffold(
       modifier = Modifier.fillMaxSize(),
-      topBar = { TopAppBar(title = { Text("Add Food Item") }) },
+      topBar = {
+        TopAppBar(
+            title = { Text(stringResource(id = R.string.add_food_item_title)) },
+            navigationIcon = {
+              IconButton(onClick = { navigationActions.goBack() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Go Back")
+              }
+            })
+      },
   ) { padding ->
     Column(
         modifier = Modifier.fillMaxSize().padding(padding).padding(16.dp),
@@ -55,7 +85,7 @@ fun AddFoodItemScreen(
           OutlinedTextField(
               value = foodName,
               onValueChange = { foodName = it },
-              label = { Text("Name of food") },
+              label = { Text(stringResource(id = R.string.food_name_hint)) },
               modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp))
 
           Row(
@@ -64,96 +94,46 @@ fun AddFoodItemScreen(
                 OutlinedTextField(
                     value = amount,
                     onValueChange = { amount = it },
-                    label = { Text("Amount") },
+                    label = { Text(stringResource(id = R.string.amount_hint)) },
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     modifier = Modifier.weight(1f).padding(end = 8.dp))
 
-                ExposedDropdownMenuBox(
+                DropdownFields(
+                    label = stringResource(id = R.string.unit_label),
+                    options = FoodUnit.values(),
+                    selectedOption = unit,
+                    onOptionSelected = { unit = it },
                     expanded = unitExpanded,
-                    onExpandedChange = { unitExpanded = !unitExpanded },
-                    modifier = Modifier.weight(1f)) {
-                      OutlinedTextField(
-                          value = unit.name.lowercase(),
-                          onValueChange = {},
-                          label = { Text("Unit") },
-                          readOnly = true,
-                          trailingIcon = {
-                            ExposedDropdownMenuDefaults.TrailingIcon(expanded = unitExpanded)
-                          },
-                          modifier = Modifier.menuAnchor())
-                      ExposedDropdownMenu(
-                          expanded = unitExpanded, onDismissRequest = { unitExpanded = false }) {
-                            FoodUnit.values().forEach { selectionOption ->
-                              DropdownMenuItem(
-                                  text = { Text(selectionOption.name.lowercase()) },
-                                  onClick = {
-                                    unit = selectionOption
-                                    unitExpanded = false // Close dropdown
-                                  })
-                            }
-                          }
-                    }
+                    onExpandedChange = { unitExpanded = it },
+                    optionLabel = { fromCapitalStringtoLowercaseString(it.name) },
+                    modifier_input = Modifier.weight(1f))
               }
 
           // Category dropdown
-          ExposedDropdownMenuBox(
+          DropdownFields(
+              label = stringResource(id = R.string.category_label),
+              options = FoodCategory.values(),
+              selectedOption = category,
+              onOptionSelected = { category = it },
               expanded = categoryExpanded,
-              onExpandedChange = { categoryExpanded = !categoryExpanded }) {
-                OutlinedTextField(
-                    value = category.name.lowercase(),
-                    onValueChange = {},
-                    label = { Text("Category") },
-                    readOnly = true,
-                    trailingIcon = {
-                      ExposedDropdownMenuDefaults.TrailingIcon(expanded = categoryExpanded)
-                    },
-                    modifier = Modifier.fillMaxWidth().menuAnchor())
-                ExposedDropdownMenu(
-                    expanded = categoryExpanded, onDismissRequest = { categoryExpanded = false }) {
-                      FoodCategory.values().forEach { selectionOption ->
-                        DropdownMenuItem(
-                            text = { Text(selectionOption.name.lowercase()) },
-                            onClick = {
-                              category = selectionOption
-                              categoryExpanded = false
-                            })
-                      }
-                    }
-              }
+              onExpandedChange = { categoryExpanded = it },
+              optionLabel = { fromCapitalStringtoLowercaseString(it.name) })
 
           Spacer(modifier = Modifier.height(16.dp))
 
-          ExposedDropdownMenuBox(
+          DropdownFields(
+              label = stringResource(id = R.string.location_label),
+              options = FoodStorageLocation.values(),
+              selectedOption = location,
+              onOptionSelected = { location = it },
               expanded = locationExpanded,
-              onExpandedChange = { locationExpanded = !locationExpanded }) {
-                OutlinedTextField(
-                    value = location.name.lowercase(),
-                    onValueChange = {},
-                    label = { Text("Location") },
-                    readOnly = true,
-                    trailingIcon = {
-                      ExposedDropdownMenuDefaults.TrailingIcon(expanded = locationExpanded)
-                    },
-                    modifier = Modifier.fillMaxWidth().menuAnchor())
-                ExposedDropdownMenu(
-                    expanded = locationExpanded, onDismissRequest = { locationExpanded = false }) {
-                      FoodStorageLocation.values().forEach { selectionOption ->
-                        DropdownMenuItem(
-                            text = { Text(selectionOption.name) },
-                            onClick = {
-                              location = selectionOption
-                              locationExpanded = false
-                            })
-                      }
-                    }
-              }
+              onExpandedChange = { locationExpanded = it },
+              optionLabel = { fromCapitalStringtoLowercaseString(it.name) })
 
-          // For dates a future improvement could be having a calendar interfare rather than manual
-          // input
           OutlinedTextField(
               value = expireDate,
               onValueChange = { expireDate = it },
-              label = { Text("Expire Date") },
+              label = { Text(stringResource(id = R.string.expire_date_hint)) },
               placeholder = { Text("dd/mm/yyyy") },
               modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
           )
@@ -161,7 +141,7 @@ fun AddFoodItemScreen(
           OutlinedTextField(
               value = openDate,
               onValueChange = { openDate = it },
-              label = { Text("Open Date") },
+              label = { Text(stringResource(id = R.string.open_date_hint)) },
               placeholder = { Text("dd/mm/yyyy") },
               modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
           )
@@ -169,49 +149,72 @@ fun AddFoodItemScreen(
           OutlinedTextField(
               value = buyDate,
               onValueChange = { buyDate = it },
-              label = { Text("Buy Date") },
+              label = { Text(stringResource(id = R.string.buy_date_hint)) },
               placeholder = { Text("dd/mm/yyyy") },
               modifier = Modifier.fillMaxWidth().padding(bottom = 32.dp),
           )
 
           Button(
               onClick = {
-                val foodFacts =
-                    FoodFacts(
-                        name = foodName,
-                        barcode = "",
-                        quantity = Quantity(amount.toDouble(), unit),
-                        category = category,
-                        // nutritionFacts = NutritionFacts()
-                    )
-                val newFoodItem =
-                    FoodItem(
-                        uid = foodItemViewModel.getUID(),
-                        foodFacts = foodFacts,
-                        location = location,
-                        expiryDate = formatDateToTimestamp(expireDate),
-                        openDate = formatDateToTimestamp(openDate),
-                        buyDate = formatDateToTimestamp(buyDate),
-                        // Have to add logic to determine status depending on the dates given here/
-                        // should be calculated
-                        status = FoodStatus.CLOSED)
-                houseHoldViewModel.addFoodItem(newFoodItem)
-                navigationActions.goBack()
+                errorMessages.clear()
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+                try {
+                  val expireDateParsed = sdf.parse(expireDate)
+                  val openDateParsed = sdf.parse(openDate)
+
+                  // Error if the expiration date is before the open date
+                  if (expireDateParsed.before(openDateParsed)) {
+                    errorMessages["date"] = "Expiration date cannot be before the open date."
+                  }
+                } catch (e: Exception) {
+                  errorMessages["dateFormat"] = "Invalid date format. Please use dd/mm/yyyy."
+                }
+
+                // Error is the food name field is empty
+                if (foodName.isBlank()) {
+                  errorMessages["foodName"] = "Food name cannot be empty."
+                }
+
+                // Error is the food amount is blank or not a number
+                if (amount.isBlank()) {
+                  errorMessages["amount"] = "Amount cannot be empty."
+                } else if (amount.toDoubleOrNull() == null) {
+                  errorMessages["amountFormat"] = "Amount must be a number."
+                }
+
+                if (errorMessages.isNotEmpty()) {
+                  showDialog = true
+                } else {
+                  val foodFacts =
+                      FoodFacts(
+                          name = foodName,
+                          barcode = "",
+                          quantity = Quantity(amount.toDouble(), unit),
+                          category = category)
+                  val newFoodItem =
+                      FoodItem(
+                          uid = foodItemViewModel.getUID(),
+                          foodFacts = foodFacts,
+                          location = location,
+                          expiryDate = formatDateToTimestamp(expireDate),
+                          openDate = formatDateToTimestamp(openDate),
+                          buyDate = formatDateToTimestamp(buyDate),
+                          status = FoodStatus.CLOSED)
+                  houseHoldViewModel.addFoodItem(newFoodItem)
+                  navigationActions.goBack()
+                }
               },
-              modifier = Modifier.fillMaxWidth().height(50.dp)) {
+              modifier = Modifier.fillMaxWidth().height(50.dp),
+              colors = ButtonDefaults.buttonColors(containerColor = primaryContainerLight)) {
                 Text(text = "Submit", fontSize = 18.sp)
               }
+
+          ErrorPopUp(
+              showDialog = showDialog,
+              onDismiss = { showDialog = false },
+              errorMessages = errorMessages.values.toList())
         }
   }
-}
-
-fun formatTimestampToDate(timestamp: Timestamp): String {
-  val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-  return sdf.format(timestamp.toDate())
-}
-
-fun formatDateToTimestamp(dateString: String): Timestamp {
-  val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-  val date = sdf.parse(dateString)
-  return Timestamp(date)
 }
