c4886ebcaebfa4e6319033ea907fa8f061f4854b
alextheprophecy <alexandrebourgoin23@gmail.com>
2024-10-16 14:14:58 +0200

    refactor(Recipe): replace FoodItem with new Ingredient data class for Recipe ingredients list
    -This is so that the recipe can also contain ingredients which are not in owned by the user (i.e. to buy)
    -Added new data class Ingredient instead of modifying FoodItem, since isOwned isnt a inherent property of a FoodItem

--- app/src/main/java/com/android/shelfLife/model/recipe/Recipe.kt
+++ app/src/main/java/com/android/shelfLife/model/recipe/Recipe.kt
@@ -5,9 +5,14 @@ import com.google.firebase.Timestamp
 import kotlin.time.Duration
 
 data class Recipe(
-    val name: String, // name of recipe
+    val name: String, // recipe name
     val instructions: List<String>, // instructions of recipes step by step (hence the list)
     val servings: Int, // total number of servings
-    val time: Duration, // time it take to cook
-    val ingredients : List<FoodItem> = listOf(), // ingredients in recipe todo check with Alex about this
+    val time: Duration, // time it takes to cook
+    val ingredients : List<Ingredient> = listOf(), // ingredients in recipe todo check with Alex about this
+)
+
+data class Ingredient(
+    val foodItem: FoodItem,
+    val isOwned: Boolean //false when the user does not have the ingredient (i.e. need to buy)
 )
