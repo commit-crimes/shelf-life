a70b9ce936c70e5d562ddde2b486f9de36f2d7aa
ElMaestroZ <99408085+ElMaestroZ@users.noreply.github.com>
2024-10-18 17:11:15 +0200

    fix: trying something

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,6 +118,7 @@ sonar {
         property("sonar.organization", "commit-crimes")
         property("sonar.host.url", "https://sonarcloud.io")
         // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
+        /*
         property(
             "sonar.junit.reportPaths",
             "${project.layout.buildDirectory.get()}/test-results/testDebugUnitTest/"
@@ -127,6 +128,7 @@ sonar {
             "sonar.androidLint.reportPaths",
             "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml"
         )
+         */
         // Paths to JaCoCo XML coverage report files.
         property(
             "sonar.coverage.jacoco.xmlReportPaths",
