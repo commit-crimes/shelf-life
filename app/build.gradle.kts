30e828e29adeedefe830afc0993b89aabe13024e
Ricardo Perello <ricardo.perellomas@epfl.ch>
2024-10-18 13:35:44 +0200

    fix: jacoco to sonar cloud

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,17 +117,15 @@ sonar {
         property("sonar.projectName", "shelf-life")
         property("sonar.organization", "commit-crimes")
         property("sonar.host.url", "https://sonarcloud.io")
-        // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
+        // Use wildcard to account for the 'TEST-' prefix in the file names
         property(
             "sonar.junit.reportPaths",
-            "${project.layout.buildDirectory.get()}/test-results/testDebugUnitTest/"
+            "${project.layout.buildDirectory.get()}/test-results/testDebugUnitTest/TEST-*.xml"
         )
-        // Paths to xml files with Android Lint issues. If the main flavor is changed, this file will have to be changed too.
         property(
             "sonar.androidLint.reportPaths",
             "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml"
         )
-        // Paths to JaCoCo XML coverage report files.
         property(
             "sonar.coverage.jacoco.xmlReportPaths",
             "${project.layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
