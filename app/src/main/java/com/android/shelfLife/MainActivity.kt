9dcc06214753d52121cccc52327a104b6634fb03
applecognac <kevinyangzhang2934@gmail.com>
2024-10-28 16:10:50 +0100

    test: Add some more HouseholdElement tests

--- app/src/main/java/com/android/shelfLife/MainActivity.kt
+++ app/src/main/java/com/android/shelfLife/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.android.shelfLife
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
