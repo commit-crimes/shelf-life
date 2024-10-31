9feabfb1e944831bc3ff510015c518d275ab822a
ElMaestroZ <99408085+ElMaestroZ@users.noreply.github.com>
2024-10-31 21:23:34 +0100

    fix: remove surface wrapping and re-add version updates

--- app/src/main/java/com/android/shelfLife/MainActivity.kt
+++ app/src/main/java/com/android/shelfLife/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -41,7 +40,13 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-    setContent { ShelfLifeTheme { Surface { ShelfLifeApp() } } }
+    setContent {
+      ShelfLifeTheme {
+        // Surface{
+        ShelfLifeApp()
+        // }
+      }
+    }
   }
 }
 
