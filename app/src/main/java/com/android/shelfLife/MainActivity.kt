1400caaf01b4cfc600c440499108d80413cb416d
ElMaestroZ <99408085+ElMaestroZ@users.noreply.github.com>
2024-10-31 22:34:48 +0100

    fix: reverted all library upgrades except for navigation.

--- app/src/main/java/com/android/shelfLife/MainActivity.kt
+++ app/src/main/java/com/android/shelfLife/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -41,11 +42,7 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
-      ShelfLifeTheme {
-        // Surface{
-        ShelfLifeApp()
-        // }
-      }
+      ShelfLifeTheme {Surface{ ShelfLifeApp()} }
     }
   }
 }
