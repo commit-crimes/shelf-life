0ba8a3f44a72415051f659913f1ccb643df18e06
applecognac <kevinyangzhang2934@gmail.com>
2024-11-12 16:18:47 +0100

    fix: Reformat with ktfmt

--- app/src/main/java/com/android/shelfLife/ui/utils/FoodItemDetails.kt
+++ app/src/main/java/com/android/shelfLife/ui/utils/FoodItemDetails.kt
@@ -18,40 +18,46 @@ import com.android.shelfLife.model.foodItem.FoodItem
 
 @Composable
 fun FoodItemDetails(foodItem: FoodItem) {
-    val textStyle = TextStyle(fontSize = 14.sp)
+  val textStyle = TextStyle(fontSize = 14.sp)
 
-    val formattedExpiryDate = foodItem.expiryDate?.let { formatTimestampToDate(it) } ?: "No Expiry Date"
-    val formattedOpenDate = foodItem.openDate?.let { formatTimestampToDate(it) } ?: "Not Opened"
-    val formattedBuyDate = formatTimestampToDate(foodItem.buyDate)
+  val formattedExpiryDate =
+      foodItem.expiryDate?.let { formatTimestampToDate(it) } ?: "No Expiry Date"
+  val formattedOpenDate = foodItem.openDate?.let { formatTimestampToDate(it) } ?: "Not Opened"
+  val formattedBuyDate = formatTimestampToDate(foodItem.buyDate)
 
-    ElevatedCard(
-        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp),
-        colors = CardDefaults.elevatedCardColors(containerColor = Color.White),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .background(Color.White)
-            .testTag("foodItemDetailsCard")
-    ) {
+  ElevatedCard(
+      elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp),
+      colors = CardDefaults.elevatedCardColors(containerColor = Color.White),
+      modifier =
+          Modifier.fillMaxWidth()
+              .padding(horizontal = 16.dp, vertical = 8.dp)
+              .background(Color.White)
+              .testTag("foodItemDetailsCard")) {
         Column(modifier = Modifier.padding(16.dp)) {
-            FoodItemDetailText("Category: ${foodItem.foodFacts.category.name}", "categoryText", textStyle)
-            FoodItemDetailText("Location: ${foodItem.location.name}", "locationText", textStyle)
-            FoodItemDetailText("Status: ${foodItem.status.name}", "statusText", textStyle)
-            FoodItemDetailText("Expiry Date: $formattedExpiryDate", "expiryDateText", textStyle)
-            FoodItemDetailText("Open Date: $formattedOpenDate", "openDateText", textStyle)
-            FoodItemDetailText("Buy Date: $formattedBuyDate", "buyDateText", textStyle)
-            FoodItemDetailText("Energy: ${foodItem.foodFacts.nutritionFacts.energyKcal} Kcal", "energyText", textStyle)
-            FoodItemDetailText("Proteins: ${foodItem.foodFacts.nutritionFacts.proteins} g", "proteinsText", textStyle)
-            FoodItemDetailText("Quantity: ${foodItem.foodFacts.quantity.amount} ${foodItem.foodFacts.quantity.unit}", "quantityText", textStyle)
+          FoodItemDetailText(
+              "Category: ${foodItem.foodFacts.category.name}", "categoryText", textStyle)
+          FoodItemDetailText("Location: ${foodItem.location.name}", "locationText", textStyle)
+          FoodItemDetailText("Status: ${foodItem.status.name}", "statusText", textStyle)
+          FoodItemDetailText("Expiry Date: $formattedExpiryDate", "expiryDateText", textStyle)
+          FoodItemDetailText("Open Date: $formattedOpenDate", "openDateText", textStyle)
+          FoodItemDetailText("Buy Date: $formattedBuyDate", "buyDateText", textStyle)
+          FoodItemDetailText(
+              "Energy: ${foodItem.foodFacts.nutritionFacts.energyKcal} Kcal",
+              "energyText",
+              textStyle)
+          FoodItemDetailText(
+              "Proteins: ${foodItem.foodFacts.nutritionFacts.proteins} g",
+              "proteinsText",
+              textStyle)
+          FoodItemDetailText(
+              "Quantity: ${foodItem.foodFacts.quantity.amount} ${foodItem.foodFacts.quantity.unit}",
+              "quantityText",
+              textStyle)
         }
-    }
+      }
 }
 
 @Composable
 fun FoodItemDetailText(text: String, tag: String, style: TextStyle) {
-    Text(
-        text = text,
-        style = style,
-        modifier = Modifier.testTag(tag)
-    )
+  Text(text = text, style = style, modifier = Modifier.testTag(tag))
 }
