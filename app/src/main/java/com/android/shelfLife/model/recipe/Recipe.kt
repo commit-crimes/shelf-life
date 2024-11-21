effbfa61da93d78f85d1d7babfa7c3d620d96b9a
alextheprophecy <alexandrebourgoin23@gmail.com>
2024-11-21 13:23:53 +0100

    refactor: update Recipes servings and new data class RecipePrompt

--- app/src/main/java/com/android/shelfLife/model/recipe/Recipe.kt
+++ app/src/main/java/com/android/shelfLife/model/recipe/Recipe.kt
@@ -1,12 +1,14 @@
 package com.android.shelfLife.model.recipe
 
 import com.android.shelfLife.model.foodFacts.FoodFacts
+import com.android.shelfLife.model.foodFacts.NutritionFacts
+import com.android.shelfLife.model.foodItem.FoodItem
 import kotlin.time.Duration
 
 data class Recipe(
     val name: String, // recipe name
     val instructions: List<String>, // instructions of recipes step by step (hence the list)
-    val servings: Int, // total number of servings
+    val servings: Float, // total number of servings
     val time: Duration, // time it takes to cook
     val ingredients: List<Ingredient> =
         listOf(), // ingredients in recipe todo check with Alex about this
@@ -15,4 +17,14 @@ data class Recipe(
 data class Ingredient(
     val foodFacts: FoodFacts,
     val isOwned: Boolean // false when the user does not have the ingredient (i.e. need to buy)
+)
+
+data class RecipePrompt (
+    val name: String,
+    val specialInstruction: String,
+    val ingredients: List<FoodItem>,
+    val missingIngredients: List<String>,
+    val servings: Float,
+    val shortDuration: Boolean,
+    val macros : NutritionFacts
 )
