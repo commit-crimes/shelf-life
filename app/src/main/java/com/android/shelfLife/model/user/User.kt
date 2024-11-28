6f5f3a215f02c27a6911a49b66f79044c9c4434c
Mateus Vital Nabholz <145538355+matvitn@users.noreply.github.com>
2024-11-28 14:19:40 +0100

    Feat/225 create model for user (#226)
    * feat: add user struct and model so that users (as well as all variables associated with a user) can be added to and fetched from firebase
    
    * chore: format using ktfmtFormat
    
    ---------
    
    Co-authored-by: Mateus Vital Nabholz <mateus.vitaln@icloud.com>

---
+++ app/src/main/java/com/android/shelfLife/model/user/User.kt
@@ -0,0 +1,21 @@
+package com.android.shelfLife.model.user
+
+data class User(
+    val uid: String,
+    val username: String,
+    val email: String,
+    val householdUIDs: List<String> = emptyList(),
+    val recipeUIDs: List<String> = emptyList(),
+    val invitationUIDs: List<String> = emptyList()
+    // TODO add macros param if we decide to do it
+) {
+  override fun toString(): String {
+    return "User:(" +
+        "UID: ${uid}\n" +
+        "Username: ${username}\n" +
+        "Email: ${email}\n" +
+        "Household UIDs: ${householdUIDs.toString()}\n" +
+        "Recipe UIDs: ${recipeUIDs.toString()}\n" +
+        "Invitation UIDs: ${invitationUIDs.toString()}\n)"
+  }
+}
