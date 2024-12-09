37d3fe2be1ee3fd3aaa2be203c28e46719ea488c
Ricardo Perello <ricardo.perellomas@epfl.ch>
2024-12-09 14:51:02 +0100

    feat(test): added hilt test runner class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,9 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        // Use HiltTestRunner for instrumentation tests
+        testInstrumentationRunner = "com.android.shelfLife.HiltTestRunner"
+
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -50,14 +52,15 @@ android {
             isIncludeAndroidResources = true
         }
     }
-    signingConfigs{
+
+    signingConfigs {
         val isCI = System.getenv("GITHUB_ACTIONS")?.toBoolean() ?: false
-        if(isCI && System.getenv("GITHUB_WORKFLOW") == "Build Android APK"){
-            named("debug"){
+        if (isCI && System.getenv("GITHUB_WORKFLOW") == "Build Android APK") {
+            named("debug") {
                 storeFile = file("../keystore.jks")
                 storePassword = System.getenv("DEBUG_KEYSTORE_PASSWORD")
-                keyAlias  = System.getenv("DEBUG_KEY_ALIAS")
-                keyPassword  = System.getenv("DEBUG_KEY_PASSWORD")
+                keyAlias = System.getenv("DEBUG_KEY_ALIAS")
+                keyPassword = System.getenv("DEBUG_KEY_PASSWORD")
             }
         }
     }
@@ -123,10 +126,8 @@ android {
         res.setSrcDirs(emptyList<File>())
         resources.setSrcDirs(emptyList<File>())
     }
-
 }
 
-// Jacoco test configuration
 tasks.withType<Test> {
     configure<JacocoTaskExtension> {
         isIncludeNoLocationClasses = true
@@ -134,104 +135,6 @@ tasks.withType<Test> {
     }
 }
 
-
-
-sonar {
-    properties {
-        property("sonar.projectKey", "commit-crimes")
-        property("sonar.projectName", "shelf-life")
-        property("sonar.organization", "commit-crimes")
-        property("sonar.host.url", "https://sonarcloud.io")
-        // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
-        property(
-            "sonar.junit.reportPaths",
-            "${project.layout.buildDirectory.get()}/test-results/testDebugUnitTest"
-        )
-        // Paths to xml files with Android Lint issues. If the main flavor is changed, this file will have to be changed too.
-        property(
-            "sonar.androidLint.reportPaths",
-            "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml"
-        )
-        // Paths to JaCoCo XML coverage report files.
-        property(
-            "sonar.coverage.jacoco.xmlReportPaths",
-            "${project.layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
-        )
-    }
-}
-
-// Jacoco test report task
-tasks.register("jacocoTestReport", JacocoReport::class) {
-    mustRunAfter("testDebugUnitTest", "connectedDebugAndroidTest")
-
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-    }
-
-    val fileFilter = listOf(
-        "**/R.class",
-        "**/R$*.class",
-        "**/BuildConfig.*",
-        "**/Manifest*.*",
-        "**/*Test*.*",
-        "android/**/*.*"
-    )
-    val debugTree = fileTree("${project.buildDir}/tmp/kotlin-classes/debug") {
-        exclude(fileFilter)
-    }
-    val mainSrc = "${project.projectDir}/src/main/java"
-
-    sourceDirectories.setFrom(files(mainSrc))
-    classDirectories.setFrom(files(debugTree))
-    executionData.setFrom(fileTree(project.buildDir) {
-        include("outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec")
-        include("outputs/code_coverage/debugAndroidTest/connected/*/coverage.ec")
-    })
-}
-
-// Ensure ktfmtCheck tasks depend on their corresponding ktfmtFormat tasks
-tasks.named("ktfmtCheckAndroidTest") {
-    dependsOn(tasks.named("ktfmtFormatAndroidTest"))
-}
-
-tasks.named("ktfmtCheckMain") {
-    dependsOn(tasks.named("ktfmtFormatMain"))
-}
-
-tasks.named("ktfmtCheckTest") {
-    dependsOn(tasks.named("ktfmtFormatTest"))
-}
-
-// Task to copy APKs to app/releases/
-tasks.register<Copy>("copyApks") {
-    // Ensure the APK is built before copying
-    dependsOn("assembleDebug")
-
-    val sourceDir = "$buildDir/outputs/apk/debug/"
-    val destinationDir = "$projectDir/releases/"
-
-    from(sourceDir)
-    into(destinationDir)
-
-    include("*.apk")
-
-    doFirst {
-        println("Listing APK files in source directory: $sourceDir")
-        fileTree(sourceDir).forEach {
-            println("Found file: ${it.name}")
-        }
-    }
-
-    doLast {
-        println("APKs copied to $destinationDir")
-        fileTree(destinationDir).forEach {
-            println("Copied APK: ${it.name}")
-        }
-    }
-}
-
-// Dependencies
 dependencies {
     // Core dependencies
     implementation(libs.androidx.core.ktx)
@@ -250,19 +153,10 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.camera.core)
-    implementation(libs.androidx.navigation.testing)
-    implementation(libs.androidx.datastore.core.android)
-    implementation(libs.androidx.datastore.preferences)
-    testImplementation(libs.test.core.ktx)
+    implementation(libs.androidx.runner)
+    implementation(libs.hilt.android.testing)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.material)
-    implementation(libs.androidx.material.icons.extended)
-
-    // OpenAI
-    implementation(libs.aallam.openai.client)
-    implementation(libs.ktor.client.apache5)
 
     // Barcode Scanner
     implementation(libs.mlkit.barcode.scanning)
@@ -276,9 +170,6 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
 
-    // Google Service
-    implementation(libs.play.services.auth)
-
     // Firebase
     implementation(libs.firebase.database.ktx)
     implementation(libs.firebase.firestore)
@@ -287,43 +178,33 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(platform(libs.firebase.bom))
 
-    // Networking with OkHttp
+    // Networking
     implementation(libs.okhttp)
 
-    // Image from network
+    // Image loading
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
 
-    // Testing Unit
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.mockk)
-    androidTestImplementation(libs.mockk.android)
-    androidTestImplementation(libs.mockk.agent)
-    testImplementation(libs.json)
-    testImplementation(libs.mockwebserver)
-
-    // Test UI
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.espresso.intents)
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    testImplementation(libs.mockito.core)
-    testImplementation(libs.mockito.inline)
-    testImplementation(libs.mockito.kotlin)
-    androidTestImplementation(libs.mockito.android)
-    androidTestImplementation(libs.mockito.kotlin)
-    testImplementation(libs.robolectric)
-    androidTestImplementation(libs.kaspresso)
-    androidTestImplementation(libs.kaspresso.allure.support)
-    androidTestImplementation(libs.kaspresso.compose.support)
-
-    // Coroutine Testing
-    testImplementation(libs.kotlinx.coroutines.test)
-
+    // Hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
-    implementation(libs.androidx.hilt.navigation.fragment)
+
+    // Hilt Testing
+    androidTestImplementation(libs.hilt.android.testing)
+    kaptAndroidTest(libs.hilt.android.compiler)
+    testImplementation(libs.hilt.android.testing)
+    kaptTest(libs.hilt.android.compiler)
+
+    // Unit Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.mockk)
+
+    // UI Testing
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
 }
 
 kapt {
