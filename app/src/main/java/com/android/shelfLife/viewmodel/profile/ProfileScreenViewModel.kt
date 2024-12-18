1d952fcd7a503f9ddf6e9690a3b0ef7e2cb0e251
Dearop <rocamar564p@gmail.com>
2024-12-18 18:12:30 +0100

    test : clean up

--- app/src/main/java/com/android/shelfLife/viewmodel/profile/ProfileScreenViewModel.kt
+++ app/src/main/java/com/android/shelfLife/viewmodel/profile/ProfileScreenViewModel.kt
@@ -1,11 +1,9 @@
 package com.android.shelfLife.viewmodel.profile
 
 import android.content.Context
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import com.android.shelfLife.model.invitations.InvitationRepository
 import com.android.shelfLife.model.user.UserRepository
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -13,8 +11,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
 @HiltViewModel
 class ProfileScreenViewModel
