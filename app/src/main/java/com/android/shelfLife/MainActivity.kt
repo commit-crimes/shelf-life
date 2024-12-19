b4414fa7a4cafc8b9a64e40feece0932be64c92f
Ricardo Perello <ricardo.perellomas@epfl.ch>
2024-12-20 00:19:20 +0100

    docs: deleted old unit tests

--- app/src/main/java/com/android/shelfLife/MainActivity.kt
+++ app/src/main/java/com/android/shelfLife/MainActivity.kt
@@ -37,9 +37,17 @@ import com.android.shelfLife.ui.recipes.generateRecipe.GenerateRecipeScreen
 import com.example.compose.ShelfLifeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * Main activity for the ShelfLife application.
+ */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+  /**
+   * Called when the activity is starting. This is where most initialization should go.
+   *
+   * @param savedInstanceState If the activity is being re-initialized after previously being shut down then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle).
+   */
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
@@ -47,6 +55,9 @@ class MainActivity : ComponentActivity() {
   }
 }
 
+/**
+ * Composable function that sets up the navigation for the ShelfLife application.
+ */
 @Composable
 fun ShelfLifeApp() {
   val navController = rememberNavController()
@@ -57,8 +68,8 @@ fun ShelfLifeApp() {
   NavHost(navController = navController, startDestination = Route.AUTH) {
     // Authentication route
     navigation(
-        startDestination = Screen.AUTH,
-        route = Route.AUTH,
+      startDestination = Screen.AUTH,
+      route = Route.AUTH,
     ) {
       composable(Screen.AUTH) { SignInScreen(navigationActions) }
     }
@@ -78,8 +89,8 @@ fun ShelfLifeApp() {
       composable(Screen.BARCODE_SCANNER) { BarcodeScannerScreen(navigationActions) }
     }
     navigation(
-        startDestination = Screen.RECIPES,
-        route = Route.RECIPES,
+      startDestination = Screen.RECIPES,
+      route = Route.RECIPES,
     ) {
       composable(Screen.RECIPES) { RecipesScreen(navigationActions) }
       composable(Screen.INDIVIDUAL_RECIPE) { IndividualRecipeScreen(navigationActions) }
