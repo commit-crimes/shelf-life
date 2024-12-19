2b749fa5b59574890c575c47b08f3d31b71f52fb
Ricardo Perello <ricardo.perellomas@epfl.ch>
2024-12-19 22:53:02 +0100

    docs: model/foodfacts

--- app/src/main/java/com/android/shelfLife/model/foodFacts/FoodFacts.kt
+++ app/src/main/java/com/android/shelfLife/model/foodFacts/FoodFacts.kt
@@ -13,6 +13,7 @@ package com.android.shelfLife.model.foodFacts
  * @property quantity The quantity of the product (e.g., weight, volume, or count).
  * @property category The category of the food item (e.g., FRUIT, VEGETABLE, MEAT).
  * @property nutritionFacts Detailed nutritional information per 100g/ml of the product.
+ * @property imageUrl The URL of the image representing the food item.
  */
 data class FoodFacts(
     var name: String, // Name of the food item
@@ -22,75 +23,95 @@ data class FoodFacts(
     val nutritionFacts: NutritionFacts = NutritionFacts(), // Default empty NutritionFacts object
     val imageUrl: String = DEFAULT_IMAGE_URL // New property for image URL
 ) {
-  companion object {
-    const val DEFAULT_IMAGE_URL =
-        "https://media.istockphoto.com/id/1354776457/vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo.jpg?s=612x612&w=0&k=20&c=w3OW0wX3LyiFRuDHo9A32Q0IUMtD4yjXEvQlqyYk9O4="
-  }
+    companion object {
+        const val DEFAULT_IMAGE_URL =
+            "https://media.istockphoto.com/id/1354776457/vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo.jpg?s=612x612&w=0&k=20&c=w3OW0wX3LyiFRuDHo9A32Q0IUMtD4yjXEvQlqyYk9O4="
+    }
 
-  override fun toString(): String {
-    return "Name: $name\n" +
-        "Barcode: $barcode\n" +
-        "Quantity: $quantity\n" +
-        "Category: ${category.name}\n" +
-        "Nutrition facts: $nutritionFacts"
-  }
+    override fun toString(): String {
+        return "Name: $name\n" +
+                "Barcode: $barcode\n" +
+                "Quantity: $quantity\n" +
+                "Category: ${category.name}\n" +
+                "Nutrition facts: $nutritionFacts"
+    }
 }
 
+/**
+ * Represents the quantity of a food item.
+ *
+ * @property amount The amount of the quantity.
+ * @property unit The unit of the quantity (e.g., GRAM, ML, COUNT).
+ */
 data class Quantity(
     var amount: Double, // Amount of the quantity
     val unit: FoodUnit = FoodUnit.GRAM // Unit of the quantity
 ) {
-  override fun toString(): String {
-    return when (unit) {
-      FoodUnit.GRAM -> {
-        if (amount >= 1000) {
-          val convertedAmount = amount / 1000
-          "${convertedAmount.toIntIfWhole()}kg"
-        } else {
-          "${amount.toIntIfWhole()}g"
+    override fun toString(): String {
+        return when (unit) {
+            FoodUnit.GRAM -> {
+                if (amount >= 1000) {
+                    val convertedAmount = amount / 1000
+                    "${convertedAmount.toIntIfWhole()}kg"
+                } else {
+                    "${amount.toIntIfWhole()}g"
+                }
+            }
+            FoodUnit.ML -> {
+                if (amount >= 1000) {
+                    val convertedAmount = amount / 1000
+                    "${convertedAmount.toIntIfWhole()}L"
+                } else {
+                    "${amount.toIntIfWhole()}ml"
+                }
+            }
+            FoodUnit.COUNT -> {
+                "${amount.toInt()} in stock"
+            }
         }
-      }
-      FoodUnit.ML -> {
-        if (amount >= 1000) {
-          val convertedAmount = amount / 1000
-          "${convertedAmount.toIntIfWhole()}L"
-        } else {
-          "${amount.toIntIfWhole()}ml"
-        }
-      }
-      FoodUnit.COUNT -> {
-        "${amount.toInt()} in stock"
-      }
     }
-  }
 
-  // Helper extension to display whole numbers without ".0"
-  private fun Double.toIntIfWhole(): String {
-    return if (this % 1.0 == 0.0) this.toInt().toString() else this.toString()
-  }
+    // Helper extension to display whole numbers without ".0"
+    private fun Double.toIntIfWhole(): String {
+        return if (this % 1.0 == 0.0) this.toInt().toString() else this.toString()
+    }
 }
 
+/**
+ * Enum class representing the unit of a food item's quantity.
+ */
 enum class FoodUnit {
-  GRAM, // Gram unit
-  ML, // Milliliter unit
-  COUNT // Item count (2 pineapples, 3 apples, etc.)
+    GRAM, // Gram unit
+    ML, // Milliliter unit
+    COUNT // Item count (2 pineapples, 3 apples, etc.)
 }
 
+/**
+ * Enum class representing the category of a food item.
+ */
 enum class FoodCategory {
-  FRUIT, // Fruit category
-  VEGETABLE, // Vegetable category
-  MEAT, // Meat category
-  DAIRY, // Dairy category
-  GRAIN, // Grain category
-  BEVERAGE, // Beverage category
-  SNACK, // Snack category
-  FISH, // Fish category
-  OTHER // Other category
+    FRUIT, // Fruit category
+    VEGETABLE, // Vegetable category
+    MEAT, // Meat category
+    DAIRY, // Dairy category
+    GRAIN, // Grain category
+    BEVERAGE, // Beverage category
+    SNACK, // Snack category
+    FISH, // Fish category
+    OTHER // Other category
 }
 
 /**
  * This data class represents the nutrition facts for a food item per 100g/100ml, with default
  * values.
+ *
+ * @property energyKcal The energy content in kilocalories.
+ * @property fat The fat content in grams.
+ * @property saturatedFat The saturated fat content in grams.
+ * @property carbohydrates The carbohydrate content in grams.
+ * @property sugars The sugar content in grams.
+ * @property proteins The protein content in grams.
+ * @property salt The salt content in grams.
  */
 data class NutritionFacts(
     val energyKcal: Int = 0, // Default energy in kilocalories is 0Kcal if not provided
@@ -101,13 +122,13 @@ data class NutritionFacts(
     val proteins: Double = 0.0, // Default proteins value is 0.0g
     val salt: Double = 0.0, // Default salt value is 0.0g
 ) {
-  override fun toString(): String {
-    return "Energy: $energyKcal Kcal\n" +
-        "Fat: $fat g\n" +
-        "Saturated fat: $saturatedFat g\n" +
-        "Carbohydrates: $carbohydrates g\n" +
-        "Sugars: $sugars g\n" +
-        "Proteins: $proteins g\n" +
-        "Salt: $salt g"
-  }
+    override fun toString(): String {
+        return "Energy: $energyKcal Kcal\n" +
+                "Fat: $fat g\n" +
+                "Saturated fat: $saturatedFat g\n" +
+                "Carbohydrates: $carbohydrates g\n" +
+                "Sugars: $sugars g\n" +
+                "Proteins: $proteins g\n" +
+                "Salt: $salt g"
+    }
 }
--- app/src/main/java/com/android/shelfLife/model/foodFacts/FoodFactsRepository.kt
+++ app/src/main/java/com/android/shelfLife/model/foodFacts/FoodFactsRepository.kt
@@ -10,36 +10,58 @@ import kotlinx.coroutines.flow.StateFlow
  * Barcode class contains the barcode of the food item.
  */
 sealed class FoodSearchInput {
-  // string search query
-  data class Barcode(val barcode: Long) : FoodSearchInput()
+    // string search query
+    data class Barcode(val barcode: Long) : FoodSearchInput()
 
-  data class Query(val searchQuery: String) : FoodSearchInput()
+    data class Query(val searchQuery: String) : FoodSearchInput()
 }
 
 interface FoodFactsRepository {
 
-  val searchStatus: StateFlow<SearchStatus>
+    /**
+     * StateFlow representing the current search status.
+     */
+    val searchStatus: StateFlow<SearchStatus>
 
-  fun resetSearchStatus()
+    /**
+     * Resets the search status to its initial state.
+     */
+    fun resetSearchStatus()
 
-  fun setFailureStatus()
+    /**
+     * Sets the search status to a failure state.
+     */
+    fun setFailureStatus()
 
-  val foodFactsSuggestions: StateFlow<List<FoodFacts>>
+    /**
+     * StateFlow representing the list of food facts suggestions.
+     */
+    val foodFactsSuggestions: StateFlow<List<FoodFacts>>
 
-  /**
-   * Searches for food facts based on the provided search input.
-   *
-   * @param searchInput The search input to use for querying food facts.
-   * @param onSuccess Callback function to handle successful search results.
-   * @param onFailure Callback function to handle search failures.
-   */
-  fun searchFoodFacts(
-      searchInput: FoodSearchInput,
-      onSuccess: (List<FoodFacts>) -> Unit,
-      onFailure: (Exception) -> Unit
-  )
+    /**
+     * Searches for food facts based on the provided search input.
+     *
+     * @param searchInput The search input to use for querying food facts.
+     * @param onSuccess Callback function to handle successful search results.
+     * @param onFailure Callback function to handle search failures.
+     */
+    fun searchFoodFacts(
+        searchInput: FoodSearchInput,
+        onSuccess: (List<FoodFacts>) -> Unit,
+        onFailure: (Exception) -> Unit
+    )
 
-  fun searchByBarcode(barcode: Long)
+    /**
+     * Searches for food facts based on the provided barcode.
+     *
+     * @param barcode The barcode to use for querying food facts.
+     */
+    fun searchByBarcode(barcode: Long)
 
-  fun searchByQuery(query: String)
+    /**
+     * Searches for food facts based on the provided query string.
+     *
+     * @param query The query string to use for querying food facts.
+     */
+    fun searchByQuery(query: String)
 }
