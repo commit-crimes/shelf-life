0ffcb8ba1d394a6d78e9a0f74b1ac25de0bff7b8
Alejandro Meredith Romero <alejandro.meredithromero@epfl.ch>
2024-12-19 19:09:39 +0100

    test : add tests for the RecipesViewModel

--- app/src/test/java/com/android/shelflife/viewmodel/recipes/IndividualRecipeViewModelTest.kt
+++ app/src/test/java/com/android/shelflife/viewmodel/recipes/IndividualRecipeViewModelTest.kt
@@ -15,6 +15,7 @@ import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import junit.framework.Assert.assertTrue
+import kotlin.time.Duration.Companion.minutes
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -27,115 +28,106 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import kotlin.time.Duration.Companion.minutes
 
 @HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
 @Config(application = dagger.hilt.android.testing.HiltTestApplication::class)
 @ExperimentalCoroutinesApi
 class IndividualRecipeViewModelTest {
-    @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
+  @get:Rule val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private val testDispatcher = StandardTestDispatcher()
+  private val testDispatcher = StandardTestDispatcher()
 
-    @MockK
-    private lateinit var mockUserRepository: UserRepository
+  @MockK private lateinit var mockUserRepository: UserRepository
 
-    @MockK
-    private lateinit var mockRecipeRepository: RecipeRepository
+  @MockK private lateinit var mockRecipeRepository: RecipeRepository
 
-    private lateinit var viewModel: IndividualRecipeViewModel
+  private lateinit var viewModel: IndividualRecipeViewModel
 
-    private val recipe1 =
+  private val recipe1 =
+      Recipe(
+          uid = "recipe1",
+          name = "Pasta Bolognese",
+          instructions = listOf("Cook  meat", " boil water", "add pasta", "eat"),
+          servings = 2F,
+          time = 45.minutes,
+          ingredients =
+              listOf(
+                  Ingredient("Pasta", Quantity(200.0, FoodUnit.GRAM)),
+                  Ingredient("Ground Beef", Quantity(100.0, FoodUnit.GRAM)),
+                  Ingredient("Tomato Sauce", Quantity(100.0, FoodUnit.ML)),
+                  Ingredient("Onion", Quantity(2.0, FoodUnit.COUNT))),
+          recipeType = RecipeType.PERSONAL)
+
+  @Before
+  fun setUp() {
+    MockKAnnotations.init(this)
+    Dispatchers.setMain(testDispatcher)
+
+    val userFlow =
+        MutableStateFlow<User>(
+            User(
+                uid = "user1",
+                username = "User 1",
+                email = "testemail@test.com",
+                selectedHouseholdUID = "test-household-uid",
+                householdUIDs = listOf("test-household-uid"),
+                recipeUIDs = listOf("recipe1", "recipe2", "recipe3")))
+
+    every { mockUserRepository.user } returns userFlow
+
+    val recipe2 =
         Recipe(
-            uid = "recipe1",
-            name = "Pasta Bolognese",
-            instructions = listOf("Cook  meat", " boil water", "add pasta", "eat"),
+            uid = "recipe2",
+            name = "Pizza",
+            instructions = listOf("Heat up the oven", "Place pizza until cooked", "eat"),
             servings = 2F,
-            time = 45.minutes,
-            ingredients =
-            listOf(
-                Ingredient("Pasta", Quantity(200.0, FoodUnit.GRAM)),
-                Ingredient("Ground Beef", Quantity(100.0, FoodUnit.GRAM)),
-                Ingredient("Tomato Sauce", Quantity(100.0, FoodUnit.ML)),
-                Ingredient("Onion", Quantity(2.0, FoodUnit.COUNT))
-            ),
-            recipeType = RecipeType.PERSONAL)
+            time = 25.minutes,
+            ingredients = listOf(Ingredient("Pizza", Quantity(1.0, FoodUnit.COUNT))),
+            recipeType = RecipeType.BASIC)
 
-    @Before
-    fun setUp(){
-        MockKAnnotations.init(this)
-        Dispatchers.setMain(testDispatcher)
-
-        val userFlow  =
-            MutableStateFlow<User>(
-                User(uid = "user1",
-                    username = "User 1",
-                    email = "testemail@test.com",
-                    selectedHouseholdUID = "test-household-uid",
-                    householdUIDs = listOf("test-household-uid"),
-                    recipeUIDs = listOf("recipe1", "recipe2", "recipe3"))
-            )
-
-        every{ mockUserRepository.user} returns userFlow
-
-        val recipe2 =
-            Recipe(
-                uid = "recipe2",
-                name = "Pizza",
-                instructions = listOf("Heat up the oven", "Place pizza until cooked", "eat"),
-                servings = 2F,
-                time = 25.minutes,
-                ingredients = listOf(Ingredient("Pizza", Quantity(1.0, FoodUnit.COUNT))),
-                recipeType = RecipeType.BASIC)
-
-        val recipe3 = Recipe(
+    val recipe3 =
+        Recipe(
             uid = "recipe3",
             name = "scrambled eggs",
             instructions = listOf("scramble eggs", "cook eggs", "eat"),
             servings = 1F,
             time = 5.minutes,
             ingredients = listOf(Ingredient("eggs", Quantity(2.0, FoodUnit.COUNT))),
-            recipeType = RecipeType.HIGH_PROTEIN
-        )
+            recipeType = RecipeType.HIGH_PROTEIN)
 
-        val recipesFlow = MutableStateFlow<List<Recipe>>(
-            listOf(recipe1, recipe2, recipe3)
-        )
+    val recipesFlow = MutableStateFlow<List<Recipe>>(listOf(recipe1, recipe2, recipe3))
 
-        every {mockRecipeRepository.recipes} returns recipesFlow
+    every { mockRecipeRepository.recipes } returns recipesFlow
 
-        val selectedRecipe = MutableStateFlow<Recipe>(recipe1)
-        every { mockRecipeRepository.selectedRecipe } returns (selectedRecipe)
+    val selectedRecipe = MutableStateFlow<Recipe>(recipe1)
+    every { mockRecipeRepository.selectedRecipe } returns (selectedRecipe)
 
-        viewModel = IndividualRecipeViewModel(
-            mockRecipeRepository, mockUserRepository
-        )
-    }
+    viewModel = IndividualRecipeViewModel(mockRecipeRepository, mockUserRepository)
+  }
 
-    @Test
-    fun `getRecipeName get the selected recipe's name`() = runTest{
-        assertTrue(viewModel.getRecipeName() == recipe1.name)
-    }
+  @Test
+  fun `getRecipeName get the selected recipe's name`() = runTest {
+    assertTrue(viewModel.getRecipeName() == recipe1.name)
+  }
 
-    @Test
-    fun `getRecipeServing gets the selected recipe's number of servings`() = runTest{
-        assertTrue(viewModel.getRecipeServing() == recipe1.servings)
-    }
+  @Test
+  fun `getRecipeServing gets the selected recipe's number of servings`() = runTest {
+    assertTrue(viewModel.getRecipeServing() == recipe1.servings)
+  }
 
-    @Test
-    fun `getRecipeTime gets the selected recipe's time`() = runTest{
-        assertTrue(viewModel.getRecipeTime() == recipe1.time.inWholeMinutes)
-    }
+  @Test
+  fun `getRecipeTime gets the selected recipe's time`() = runTest {
+    assertTrue(viewModel.getRecipeTime() == recipe1.time.inWholeMinutes)
+  }
 
-    @Test
-    fun `getRecipeIngredients gets the selected recipe's list of ingredients`() = runTest{
-        assertTrue(viewModel.getRecipeIngredients() == recipe1.ingredients)
-    }
+  @Test
+  fun `getRecipeIngredients gets the selected recipe's list of ingredients`() = runTest {
+    assertTrue(viewModel.getRecipeIngredients() == recipe1.ingredients)
+  }
 
-    @Test
-    fun `getRecipeInstruction gets the selected recipe's list of instruction`() = runTest{
-        assertTrue(viewModel.getRecipeInstruction() == recipe1.instructions)
-    }
+  @Test
+  fun `getRecipeInstruction gets the selected recipe's list of instruction`() = runTest {
+    assertTrue(viewModel.getRecipeInstruction() == recipe1.instructions)
+  }
 }
--- app/src/test/java/com/android/shelflife/viewmodel/recipes/RecipesViewModelTest.kt
+++ app/src/test/java/com/android/shelflife/viewmodel/recipes/RecipesViewModelTest.kt
@@ -19,6 +19,7 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import junit.framework.Assert.assertFalse
 import junit.framework.Assert.assertTrue
+import kotlin.time.Duration.Companion.minutes
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -32,7 +33,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import kotlin.time.Duration.Companion.minutes
 
 @HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
@@ -54,80 +54,75 @@ class RecipesViewModelTest {
   private lateinit var viewModel: RecipesViewModel
 
   @Before
-  fun setUp(){
+  fun setUp() {
     MockKAnnotations.init(this)
     Dispatchers.setMain(testDispatcher)
 
     val householdFlow =
-      MutableStateFlow<HouseHold?>(
-        HouseHold(
-          uid = "test-household-uid",
-          name = "Test Household",
-          members = listOf("user1", "user2"),
-          sharedRecipes = emptyList(),
-          ratPoints = emptyMap(),
-          stinkyPoints = emptyMap()))
+        MutableStateFlow<HouseHold?>(
+            HouseHold(
+                uid = "test-household-uid",
+                name = "Test Household",
+                members = listOf("user1", "user2"),
+                sharedRecipes = emptyList(),
+                ratPoints = emptyMap(),
+                stinkyPoints = emptyMap()))
     every { mockHouseHoldRepository.selectedHousehold } returns householdFlow
 
     every { mockHouseHoldRepository.households } returns MutableStateFlow(emptyList())
 
-    val userFlow  =
-      MutableStateFlow<User>(
-        User(uid = "user1",
-          username = "User 1",
-          email = "testemail@test.com",
-          selectedHouseholdUID = "test-household-uid",
-          householdUIDs = listOf("test-household-uid"),
-          recipeUIDs = listOf("recipe1", "recipe2", "recipe3"))
-      )
+    val userFlow =
+        MutableStateFlow<User>(
+            User(
+                uid = "user1",
+                username = "User 1",
+                email = "testemail@test.com",
+                selectedHouseholdUID = "test-household-uid",
+                householdUIDs = listOf("test-household-uid"),
+                recipeUIDs = listOf("recipe1", "recipe2", "recipe3")))
 
-    every{ mockUserRepository.user} returns userFlow
+    every { mockUserRepository.user } returns userFlow
 
     val recipe1 =
-      Recipe(
-        uid = "recipe1",
-        name = "Pasta Bolognese",
-        instructions = listOf("Cook  meat", " boil water", "add pasta", "eat"),
-        servings = 2F,
-        time = 45.minutes,
-        ingredients =
-        listOf(
-          Ingredient("Pasta", Quantity(200.0, FoodUnit.GRAM)),
-          Ingredient("Ground Beef", Quantity(100.0, FoodUnit.GRAM)),
-          Ingredient("Tomato Sauce", Quantity(100.0, FoodUnit.ML)),
-          Ingredient("Onion", Quantity(2.0, FoodUnit.COUNT))
-        ),
-        recipeType = RecipeType.PERSONAL)
+        Recipe(
+            uid = "recipe1",
+            name = "Pasta Bolognese",
+            instructions = listOf("Cook  meat", " boil water", "add pasta", "eat"),
+            servings = 2F,
+            time = 45.minutes,
+            ingredients =
+                listOf(
+                    Ingredient("Pasta", Quantity(200.0, FoodUnit.GRAM)),
+                    Ingredient("Ground Beef", Quantity(100.0, FoodUnit.GRAM)),
+                    Ingredient("Tomato Sauce", Quantity(100.0, FoodUnit.ML)),
+                    Ingredient("Onion", Quantity(2.0, FoodUnit.COUNT))),
+            recipeType = RecipeType.PERSONAL)
 
     val recipe2 =
-      Recipe(
-        uid = "recipe2",
-        name = "Pizza",
-        instructions = listOf("Heat up the oven", "Place pizza until cooked", "eat"),
-        servings = 2F,
-        time = 25.minutes,
-        ingredients = listOf(Ingredient("Pizza", Quantity(1.0, FoodUnit.COUNT))),
-        recipeType = RecipeType.BASIC)
+        Recipe(
+            uid = "recipe2",
+            name = "Pizza",
+            instructions = listOf("Heat up the oven", "Place pizza until cooked", "eat"),
+            servings = 2F,
+            time = 25.minutes,
+            ingredients = listOf(Ingredient("Pizza", Quantity(1.0, FoodUnit.COUNT))),
+            recipeType = RecipeType.BASIC)
 
-    val recipe3 = Recipe(
-      uid = "recipe3",
-      name = "scrambled eggs",
-      instructions = listOf("scramble eggs", "cook eggs", "eat"),
-      servings = 1F,
-      time = 5.minutes,
-      ingredients = listOf(Ingredient("eggs", Quantity(2.0, FoodUnit.COUNT))),
-      recipeType = RecipeType.HIGH_PROTEIN
-    )
+    val recipe3 =
+        Recipe(
+            uid = "recipe3",
+            name = "scrambled eggs",
+            instructions = listOf("scramble eggs", "cook eggs", "eat"),
+            servings = 1F,
+            time = 5.minutes,
+            ingredients = listOf(Ingredient("eggs", Quantity(2.0, FoodUnit.COUNT))),
+            recipeType = RecipeType.HIGH_PROTEIN)
 
-    val recipesFlow = MutableStateFlow<List<Recipe>>(
-      listOf(recipe1, recipe2, recipe3)
-    )
+    val recipesFlow = MutableStateFlow<List<Recipe>>(listOf(recipe1, recipe2, recipe3))
 
-    every {mockRecipeRepository.recipes} returns recipesFlow
+    every { mockRecipeRepository.recipes } returns recipesFlow
 
-    viewModel = RecipesViewModel(
-      mockUserRepository, mockRecipeRepository, mockHouseHoldRepository
-    )
+    viewModel = RecipesViewModel(mockUserRepository, mockRecipeRepository, mockHouseHoldRepository)
   }
 
   @Test
@@ -145,29 +140,29 @@ class RecipesViewModelTest {
   }
 
   @Test
-  fun `changeQuery changes query to the new value`() = runTest{
-    //initialize that the query is null
+  fun `changeQuery changes query to the new value`() = runTest {
+    // initialize that the query is null
     assertTrue(viewModel.query.value == "")
 
     // add value Pasta to query
     viewModel.changeQuery("Pasta")
     assertTrue(viewModel.query.value == "Pasta")
 
-    //change value back to "" (i.e. deleting query, check UI)
+    // change value back to "" (i.e. deleting query, check UI)
     viewModel.changeQuery("")
     assertTrue(viewModel.query.value == "")
   }
 
   @Test
-  fun `expandFab and shrinkFab change the value of fabExpanded`()= runTest{
-    //we check that it is initially closed
+  fun `expandFab and shrinkFab change the value of fabExpanded`() = runTest {
+    // we check that it is initially closed
     assertTrue(!viewModel.fabExpanded.value)
 
     // we expand it
     viewModel.expandFab()
     assertTrue(viewModel.fabExpanded.value)
 
-    //we shrinck it back
+    // we shrinck it back
     viewModel.shrinkFab()
     assertTrue(!viewModel.fabExpanded.value)
   }
